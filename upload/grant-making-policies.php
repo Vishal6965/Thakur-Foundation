61142da3dc26880a725de64089bc58edcadc6de5
Vishal6965 <69199259+Vishal6965@users.noreply.github.com>
2020-08-04 23:46:25 +0530

    Add files via upload

---
+++ upload/grant-making-policies.php
@@ -0,0 +1,31 @@
+<div class="" id="addGrant">
+<?php
+session_start() ;
+include('header.php');
+?>
+<section id="addgrant-header1" class="header-parallax">
+    <div class="main_container">
+      <div class="tbl">
+        <div class="serbx">
+         <h1>Grants for Investigative Reporting in <br> Public Health - 2019 </h1>
+        </div>
+      </div>
+    </div>
+  </section>
+<section>
+<div class="main_container adgrant">
+<article class="">
+<h2>Grant Making Policies for Public Health</h2>
+<p><a href="#">International Grant Making Policy</a></p>
+<p><a href="#">Affidavit packet for NON-US grant applicants</a></p>
+<p><a href="#">Grant Making Affidavit</a></p>
+<p><a href="#">Grant Making Affidavit Update</a></p>
+<p><a href="#">Investigative Reporting Grant Policy</a></p>
+
+</article>
+
+
+</div>
+</section>
+</div>
+<?php  include('footer.php');?>
