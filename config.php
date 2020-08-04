6fe9bc7ef1df0370a73913100636de62a81a0083
Vishal6965 <69199259+Vishal6965@users.noreply.github.com>
2020-08-04 23:56:37 +0530

    Update config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@ $base_url="https://www.thakur-foundation.org/";
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $mysql_hostname = "localhost";
-$mysql_user = "thakurfo_main";
-$mysql_password = "Thakur123";
-$mysql_database = "thakurfo_main";
+$mysql_user = "********";
+$mysql_password = "**********";
+$mysql_database = "**********";
 $con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
 mysqli_select_db($con,$mysql_database) or die("Opps some thing went wrong in db connect");
 ?>
